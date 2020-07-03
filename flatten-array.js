889cebbca14b4f653bfd712e71702df01bc0dd46
OldDream <huangyn15@qq.com>
2020-07-03 17:13:53 +0800

    add： 递归解法

--- flatten-array.js
+++ flatten-array.js
@@ -14,5 +14,31 @@ function flattenArray(arr) {
   return result
 }
 
+/**
+ * 使用reduce
+ * @param {*} arr 
+ */
+function flattenArray2(arr) {
+  // function flatten(arr) {
+  //   return arr.reduce((pre, next) => {
+  //     if (Array.isArray(next)) { return pre.concat(flatten(next)); }
+  //     else {
+  //       if (!isNaN(next)) {
+  //         return pre.concat(next);
+  //       }
+  //     }
+  //     return pre;
+  //   }, []);
+  // }
+  const flat = (a, b) => {
+    return a.concat(Array.isArray(b) ? flattenArray2(b) : b)
+  }
+  const flatArr = arr.reduce(flat, [])
+  // const flatArr = flatten(arr)
+
+  return [...new Set(flatArr)].sort((a, b) => a - b);
+}
+
+
 var arr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10]
-console.log(flattenArray(arr))
+console.log(flattenArray2(arr))
