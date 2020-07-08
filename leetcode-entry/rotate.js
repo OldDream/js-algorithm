901b8e6dd3a466349c446b02972051fc31961a0f
OldDream <huangyn15@qq.com>
2020-07-08 19:55:19 +0800

    add：数组翻转

--- leetcode-entry/rotate.js
+++ leetcode-entry/rotate.js
@@ -4,5 +4,30 @@
  */
 const rotate = (matrix) => {
   // 两次翻转：先转置，后每一列都镜像到另一侧
-
+  // 转置 transform
+  for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+    for (let colIndex = rowIndex; colIndex < matrix[rowIndex].length; colIndex++) {
+      let temp = matrix[rowIndex][colIndex]
+      matrix[rowIndex][colIndex] = matrix[colIndex][rowIndex]
+      matrix[colIndex][rowIndex] = temp
+    }
+  }
+  // 镜像翻转,i行 j列
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length / 2; j++) {
+      let temp = matrix[i][j]
+      matrix[i][j] = matrix[i][matrix[i].length - j - 1]
+      matrix[i][matrix[i].length - j - 1] = temp
+    }
+  }
 };
+
+let twoDArr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+console.log(twoDArr)
+rotate(twoDArr)
+console.log(twoDArr)
