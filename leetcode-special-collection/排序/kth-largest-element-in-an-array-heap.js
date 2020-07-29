c183d69a6bdb2ed4d8d27ba2aa925037a4b1ea89
OldDream <huangyn15@qq.com>
2020-07-29 18:14:50 +0800

    add: 堆排序

--- leetcode-special-collection/排序/kth-largest-element-in-an-array-heap.js
+++ leetcode-special-collection/排序/kth-largest-element-in-an-array-heap.js
@@ -4,7 +4,6 @@
 // 2、小顶堆，限制大小为 k，大小到达k后，每次放入一个，就得从堆顶删除一个，最终得到一个大小为k的堆，堆顶即为所求
 // 3、大顶堆，遍历整个数组构造二叉树，构造结束后，从从顶上删除 k-1 个节点，顶部元素即为所求。
 
-
 /**
  * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/description/
  * https://github.com/sisterAn/JavaScript-Algorithms/issues/60
@@ -14,7 +13,52 @@
  * @return {number}
  */
 let findKthLargest = function (nums, k) {
-  
+  // 采用小顶堆，则堆大小为 k，先构建尺寸为k的堆，后如果后来的数值大于 堆顶， 则换掉顶 + 重新堆化
+  // 采用从后往前的堆化方式
+  const heap = buildHeap(nums, k)
+  for (let index = k; index < nums.length; index++) {
+    const currentNum = nums[index];
+    if (currentNum > heap[0]) { // 把大的往堆里塞
+      heap[0] = currentNum
+      heapify(heap)
+    }
+  }
+
+  return heap[0]
 };
+
+/**
+ * 插入建堆
+ */
+function buildHeap(sourceArr, heapSize) {
+  let heap = new Array(heapSize)
+  for (let index = 0; index < heapSize; index++) {
+    heap[index]=sourceArr[index]
+    while (Math.floor(index / 2) >= 0 && heap[index] < heap[Math.floor(index / 2)]) {
+      swap(heap, index, Math.floor(index / 2))
+    }
+  }
+  return heap
+}
+
+/**
+ * 堆化，自下而上(和父节点比较)
+ */
+function heapify(heapArr) {
+  let i = heapArr.length - 1
+  while (Math.floor(i / 2) >= 0 && heapArr[i] < heapArr[Math.floor(i / 2)]) {
+    swap(heapArr, i, Math.floor(i / 2))
+  }
+}
+
+/**
+   * 交换
+   */
+function swap(arr, i, j) {
+  let temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
+
 
 console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2))
