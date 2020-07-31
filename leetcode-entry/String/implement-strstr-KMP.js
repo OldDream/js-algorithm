6b9338ed492bf2db9984aa3b9c7b14ed4b133508
OldDream <huangyn15@qq.com>
2020-07-31 20:03:20 +0800

    backup

---
+++ leetcode-entry/String/implement-strstr-KMP.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode-cn.com/problems/implement-strstr/solution/kmp-suan-fa-xiang-jie-by-labuladong/
+ * KMP法
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+let strStr = (haystack, needle) => {
+  
+};
+
+// KMP 算法
+
+console.log(strStr("mississippi", 'issip'))
