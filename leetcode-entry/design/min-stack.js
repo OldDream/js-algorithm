f9f92cc80bab800c714fddb0fd1a29083d334a82
OldDream <huangyn15@qq.com>
2020-09-02 21:38:51 +0800

    add: min stack

--- leetcode-entry/design/min-stack.js
+++ leetcode-entry/design/min-stack.js
@@ -2,7 +2,8 @@
  * initialize your data structure here.
  */
 var MinStack = function() {
-
+  this.stack = []
+  this.minStack = [Number.POSITIVE_INFINITY]
 };
 
 /** 
@@ -10,28 +11,31 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(x) {
+  this.stack.push(x)
 
+  this.minStack.push(Math.min(this.minStack[this.minStack.length - 1], x))
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-
+  this.stack.pop()
+  this.minStack.pop()
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-
+  return this.stack[this.stack.length - 1]
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-  
+  return this.minStack[this.minStack.length - 1]
 };
 
 /**
