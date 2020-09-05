b67fbfef44cf2b4ef868d99c8445dd89dff9576b
Old_dream <huangyn15@qq.com>
2020-09-06 00:27:09 +0800

    add: 另一方法

--- leetcode-entry/Math/power-of-three.js
+++ leetcode-entry/Math/power-of-three.js
@@ -11,3 +11,11 @@ var isPowerOfThree = function (n) {
     return isPowerOfThree(n / 3)
   }
 };
+
+var isPowerOfThree = function (n) {
+  if (n < 1) return false;
+  while (n % 3 === 0) {
+    n /= 3
+  }
+  return n === 1
+};
