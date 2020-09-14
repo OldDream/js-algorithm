180f8dad9b91043208dfc6a581ad7bda3eda64d4
Old_dream <huangyn15@qq.com>
2020-09-15 01:15:00 +0800

    add: edition2

--- 杂项/Curry.js
+++ 杂项/Curry.js
@@ -1,9 +1,19 @@
-function add (sum) {
+function add(sum) {
   let fn = (n) => {
-    return add (n + sum)
+    return add(n + sum)
   }
   fn.valueOf = () => sum
   return fn
 }
 
-console.log(+add(1)(2))
+// console.log(+add(1)(2))
+
+function add2() {
+  let argsSum = Array.from(arguments).reduce((prev, next) => prev + next)
+  let fn = function () {
+    return add2(Array.from(arguments).reduce((prev, next) => prev + next) + argsSum)
+  }
+  fn.valueOf = () => argsSum
+  return fn
+}
+console.log(+add2(1, 2)(1))
